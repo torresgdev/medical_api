aae267d53f32ba1739ec9a47e9f76faabfe6711f
Gabriel Torres <gbrtorress@gmail.com>
2025-04-15 09:07:57 -0300

    created specialty service

---
+++ src/specialties/services/specialties.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Specialty } from "../entities/specialty.entity";
+import { CreateSpecialtyDto } from "../dtos/create-specialty.dto";
+
+@Injectable()
+export class SpecialtiesService {
+  constructor(
+    @InjectRepository(Specialty)
+    private specialtyRepository: Repository<Specialty>,
+  ) {}
+
+  create(createSpecialtyDto: CreateSpecialtyDto) {
+    const specialty = this.specialtyRepository.create(createSpecialtyDto)
+    return this.specialtyRepository.save(specialty);
+  }
+
+  findAll() {
+    return this.specialtyRepository.find()
+  }
+
+  findOne(id: number) {
+    return this.specialtyRepository.findOne({
+        where: {id}
+    });
+  }
+
+  update(id: number, updateSpecialty: CreateSpecialtyDto) {
+    return this.specialtyRepository.update(id, updateSpecialty)
+  }
+
+  delete(id: number) {
+    return this.specialtyRepository.delete(id)
+  }
+
+
+}
